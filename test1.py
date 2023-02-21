b3702f7b5675a1d66d4623c854c2ac9e9b147f33
paromita2002 <dasparomita283@gmail.com>
2023-02-21 12:51:12 +0530

    update code

---
+++ test1.py
@@ -0,0 +1,53 @@
+"""class test:
+    a=float(input("Enter a number:"))
+    b=float(input("Enter second number:"))
+    xyz=a+b
+    zyx=a-b
+    yzx=a*b
+    zxy=a/b
+    print(round(xyz,2))
+    print(round(zyx,2))
+    print(round(yzx,2))
+    print(round(zxy,2))
+class test_2(test):
+    pass
+class test_3(test_2):
+    pass
+class test_4(test_3):
+    pass
+class test_5(test_4):
+    pass
+obj_inherit=test_5
+obj_inherit.sit()"""
+
+
+"""import calendar
+class Calender:
+    def main():
+        yy=int(input("Enter the Year:"))
+        mm=int(input("Enter the Month:"))
+        print(calendar.month(yy,mm))
+Calender.main()"""
+
+"""class Number:
+    def __init__(self,num):
+        self.num = num
+    def check(self):
+        if self.num>0:
+            print("positive")
+        elif self.num==0:
+            print("Zero")
+        else:            
+            print("Negative")
+num = Number(int(input("Enter number: ")))"""
+
+"""class test:
+    def main():
+        a=str(input("Write Something:"))
+        f=open("demofile.txt","x")
+        f=open("demofile.txt","w")
+        f.write(a)
+        f.close(
+            f=open"""
+        
+
